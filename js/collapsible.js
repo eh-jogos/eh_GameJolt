912ccc514f261c830b4c3385bd5f384b62a0dce9
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-09-14 07:37:12 -0300

    Update to version with properties and method tables

---
+++ js/collapsible.js
@@ -0,0 +1,31 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+    
+for (i = 0; i < coll.length; i++) {
+	if (coll[i].classList.contains("active")){
+		coll[i].innerHTML = "-"
+	} else {
+		coll[i].innerHTML = "+"
+	}
+
+	var link = coll[i].nextElementSibling;
+	coll[i].style.lineHeight = link.offsetHeight + "px";
+
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var is_active = this.classList.contains("active")
+		if (is_active){
+			this.innerHTML = "-";
+		} else {
+			this.innerHTML = "+";
+		}
+	
+		var parent = this.parentElement;
+		var content = parent.getElementsByTagName("UL")[0];
+		if (is_active) {
+			content.style.display = "block";
+		} else {
+			content.style.display = "none";
+		}
+	});
+}
